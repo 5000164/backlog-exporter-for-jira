057933e60c68f1747a822fc3ff0c262e1d6ee76f
菅原 浩 <sncts35326@gmail.com>
2018-04-21 17:05:48 +0900

    feat: Backlog のイシューを JIRA でインポートできる形式に変換

--- src/main/scala/jp/_5000164/backlog_exporter_for_jira/interfaces/Application.scala
+++ src/main/scala/jp/_5000164/backlog_exporter_for_jira/interfaces/Application.scala
@@ -4,6 +4,6 @@ import jp._5000164.backlog_exporter_for_jira.infractructure.Settings
 
 object Application extends App {
   val backlog = new Backlog(sys.env("BACKLOG_SPACE_ID"), sys.env("BACKLOG_API_KEY"))
-  val exportContent = Settings.settings.targets.map(backlog.fetchIssues)
-  println(exportContent)
+  val projects = Settings.settings.targets.map(backlog.fetchIssues)
+  println(projects)
 }
